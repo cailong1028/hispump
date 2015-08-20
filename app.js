9f2d664cad9e5296c88557a9c632ae267ac21b27
cailong <cailong@unicall.cc>
2015-08-20 16:06:03 +0800

    1: 修正了上次提交遗留的在server中执行brpop的监听操作时,只pop,未写入数据库的bug, 使用sol-redis-pool代替目前的直接redis方式, 使redis连接有效,并且可控 2: 添加了异常处理模式, 参照nodejs的首参数为err的标准,3: 修正了server在redis连接不上时挂掉的bug,目前改为redis挂了的话也依然监听,当redis监听到之后,再次连接, 4: 添加了根据当前node生产或者开发环境写入不同redis库的配置, 目前的配置是开发环境写入数据库0, 生产环境写入数据库2,在生产环境部署的时候需要设置机器NODE_ENV环境变量为production

--- app.js
+++ app.js
@@ -9,10 +9,21 @@ var routes = require('./lib/routes/index');
 var users = require('./lib/routes/users');
 var upload = require('./lib/routes/upload');
 
+var app = express();
+
+//env
+global.env = 'development';
+if (process.env.NODE_ENV && process.env.NODE_ENV) {
+	global.env = process.env.NODE_ENV;
+}
+
+//TODO 如果是生产环境, 先替换生产环境配置文件,然后启动服务
+if(global.env === 'production'){
+
+}
+//brpop UploadChannel
 var redisBrpopTask = require('./lib/biz/redisBrpopTask');
 redisBrpopTask();
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,7 +33,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -31,10 +42,10 @@ app.use('/users', users);
 app.use('/upload', upload);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -42,23 +53,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function (err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 
